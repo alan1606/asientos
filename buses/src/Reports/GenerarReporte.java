1014a8da32de8298ff1a01e3ce35176b84127446
Alan Aguirre <62415210+alan1606@users.noreply.github.com>
2021-09-30 13:58:10 -0600

    Agregado el detalle de las habitaciones

--- buses/src/Reports/GenerarReporte.java
+++ buses/src/Reports/GenerarReporte.java
@@ -1,11 +1,14 @@
 package Reports;
 
 import ClassDAO.DetalleDAO;
+import ClassDAO.DetalleHotelDestinoViajeDAO;
+import ClassVO.DetalleHotelDestinoViajeVO;
 import ClassVO.DetalleVO;
 import Conexion.Conexion;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import javax.swing.JOptionPane;
 import net.sf.jasperreports.engine.*;
@@ -15,13 +18,19 @@ import net.sf.jasperreports.view.JasperViewer;
 public class GenerarReporte {
 
     public static void reporteTicket(int idCliente, int idViaje, String cliente, String ciudad, String estado, String fecha, Long idDetalle) {
-        String reporteDir = "ReporteTicket.jasper";
-        DetalleDAO modelo = new DetalleDAO();
-        DetalleVO detalle = modelo.encontrar(idDetalle);
-
+        String reporteDir = "ReporteTicket.jasper"; //El reporte por defecto, es viaje redondo, sin habitaciones
+        DetalleDAO modelo = new DetalleDAO(); //Conector a detalles 
+        DetalleVO detalle = modelo.encontrar(idDetalle); //Encontramos ese detalle para poder saber si es viaje redondo o sencillo
+        DetalleHotelDestinoViajeDAO modeloHabitaciones = new DetalleHotelDestinoViajeDAO(); //Conector hacia esa tabla de la db, nos va a arrojar cuántas hanitaciones compró en x hotel
+        List<DetalleHotelDestinoViajeVO> detalleHabitaciones = modeloHabitaciones.encontrarDetalle(idDetalle); //Encontramos ese detalle
+        
         Connection conn = null;
         if (detalle.getViaje().equals("SENCILLO")) {
             reporteDir = "ReporteTicketSencillo.jasper";
+        }
+        //Es viaje redondo, es un hecho, sólo hay que saber si tiene por lo menos una habitación
+        else if(!detalleHabitaciones.isEmpty()){  //Si no está vacio el detalle de habitaciones
+            reporteDir = "ReporteTicketRedondoConHabitaciones.jasper";//Entonces va a ser un ticket con habitaciones
         }
         try {
             conn = Conexion.getConnection();
